c63d1249d71b584714087b71346bdfd44c16a34d
Riciere Marcone <85197053+ricichien@users.noreply.github.com>
2024-12-19 14:16:10 -0300

    commiting files

---
+++ src/views/ProductView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Product</h2>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+console.log(route.params)
+</script>
+
+<style lang="scss" scoped>
+</style>
